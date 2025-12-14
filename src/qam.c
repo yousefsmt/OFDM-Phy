3e4ba26ba9cf3064fdf6a1474a1f8dd62aaa74aa
Yousef Samet <yoosefsamet@gmail.com>
2025-12-14 09:47:07 +0330

    [feat] implement QAM modulation by adding mapping function for bits to complex symbols

---
+++ src/qam.c
@@ -0,0 +1,49 @@
+#include "qam.h"
+
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int map_bits_to_complex(const uint8_t* bits, complex_t* symbols)
+{
+    if (symbols && bits)
+    {
+        uint8_t msb_four_bit = 0;
+        uint8_t lsb_four_bit = 0;
+
+        for (size_t i = 0; i < SYMBOL_RATE; i++)
+        {
+            for (size_t k = 0; k < BITS_PER_SYM / 2; k++)
+            {
+                switch (k)
+                {
+                    case 0:
+                        msb_four_bit = MSB_4(bits[i]) >> 2u;
+                        msb_four_bit ^= msb_four_bit >> 1;
+                        lsb_four_bit = MSB_4(bits[i]) & 3u;
+                        lsb_four_bit ^= lsb_four_bit >> 1;
+                        symbols[2 * i].i = (float) AMP_CALC((int) msb_four_bit, BITS_PER_SYM);
+                        symbols[2 * i].q = (float) AMP_CALC((int) lsb_four_bit, BITS_PER_SYM);
+                        break;
+                    case 1:
+                        msb_four_bit = LSB_4(bits[i]) >> 2u;
+                        msb_four_bit ^= msb_four_bit >> 1;
+                        lsb_four_bit = LSB_4(bits[i]) & 3u;
+                        lsb_four_bit ^= lsb_four_bit >> 1;
+                        symbols[2 * i + 1].i = (float) AMP_CALC((int) msb_four_bit, BITS_PER_SYM);
+                        symbols[2 * i + 1].q = (float) AMP_CALC((int) lsb_four_bit, BITS_PER_SYM);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+    else
+    {
+        fprintf(stderr, "cannot malloc complex array\n");
+        return -1;
+    }
+
+    return 0;
+}
